aabe4028f44fd0a69f4f242e78b18fa699b7c808
MariangelMoraMarquez <mariangelmoramarquez.com>
2020-02-27 15:10:25 -0500

    Portafolio

---
+++ app.js
@@ -0,0 +1,26 @@
+const event1 = document.getElementById("firsEvent");
+const event2 = document.getElementById("secondEvent");
+const event3 = document.getElementById("thirdEvent");
+const event4 = document.getElementById("fourthEvent");
+
+event1.addEventListener("click", () =>
+  window.open(
+    "https://alejandramoralesb.github.io/SCL010-data-lovers/src/index.html",
+    "_blank"
+  )
+);
+event2.addEventListener("click", () =>
+  window.open(
+    "https://github.com/AlejandraMoralesB/SCL010-Burger-Queen",
+    "_blank"
+  )
+);
+event3.addEventListener("click", () =>
+  window.open(
+    "https://carolgmonteiro.github.io/SCL010-hackathon-peliculas/src/index.html",
+    "_blank"
+  )
+);
+event4.addEventListener("click", () =>
+  window.open("https://github.com/AlejandraMoralesB/SCL010-md-links", "_blank")
+);
